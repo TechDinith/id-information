61b3f460c0389c54a6cf49777011258393cb34b8
unknown <kaushikadinith1996@gmail.com>
2022-06-05 12:20:00 +0530

    init

---
+++ src/components/pages/idInfor/idInfor.page.tsx
@@ -0,0 +1,15 @@
+import TextField from "@mui/material/TextField";
+
+const IdInforPage = () => {
+  return (
+    <div>
+      <h1>ID Information Page</h1>
+
+      <div>
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+      </div>
+    </div>
+  );
+};
+
+export default IdInforPage;
